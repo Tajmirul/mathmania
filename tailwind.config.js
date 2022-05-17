fb103aae6c8198dd384e7312f1ec3c412fef98d4
tajmirul <tasmirolislam@gmail.com>
2022-05-18 03:01:33 +0600

    home page without mobile menu

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './src/**/*.css',
+    './*.html'
+  ],
+  theme: {
+    "fontFamily": {
+      "poppins": ["Poppins", "sans-serif"],
+      "merriweather": ["Merriweather", "serif"],
+    },
+    extend: {
+      colors: {
+        'primary': '#21a7d0',
+        "secondary": "#273c66",
+      },
+    },
+  },
+  plugins: [],
+}
